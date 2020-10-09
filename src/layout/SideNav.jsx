6a3d42d95bbe4a3f361b7086c6ed53958ad972b7
Gerardo Villa Ramírez <gerardovr@MacBook-Pro.local>
2020-10-08 21:14:00 -0500

    add sidenav for layout page

--- src/layout/SideNav.jsx
+++ src/layout/SideNav.jsx
@@ -1,20 +1,19 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 // Ant Design
 import { Layout, Menu } from "antd";
 import {
-  DesktopOutlined,
   PieChartOutlined,
-  FileOutlined,
   TeamOutlined,
   UserOutlined,
+  AppstoreOutlined,
 } from "@ant-design/icons";
 
 const { Sider } = Layout;
 const { SubMenu } = Menu;
 
 const SideNav = () => {
-
   const [collapsed, setCollapsed] = useState(false);
 
   const onCollapse = (collapsed) => {
@@ -26,21 +25,22 @@ const SideNav = () => {
       <div className="logo" />
       <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline">
         <Menu.Item key="1" icon={<PieChartOutlined />}>
-          Option 1
+        <Link to="/">
+          Dashboard
+          </Link>
         </Menu.Item>
-        <Menu.Item key="2" icon={<DesktopOutlined />}>
-          Option 2
-        </Menu.Item>
-        <SubMenu key="sub1" icon={<UserOutlined />} title="User">
-          <Menu.Item key="3">Tom</Menu.Item>
-          <Menu.Item key="4">Bill</Menu.Item>
-          <Menu.Item key="5">Alex</Menu.Item>
+        <SubMenu key="sub1" icon={<AppstoreOutlined />} title="Admin">
+            <Menu.Item icon={<UserOutlined />} key="2">
+          <Link to="/admin/users">
+              Usuarios
+          </Link>
+            </Menu.Item>
+          <Menu.Item icon={<TeamOutlined />} key="3">
+          <Link to="/admin/groups">
+            Grupos
+            </Link>
+          </Menu.Item>
         </SubMenu>
-        <SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
-          <Menu.Item key="6">Team 1</Menu.Item>
-          <Menu.Item key="8">Team 2</Menu.Item>
-        </SubMenu>
-        <Menu.Item key="9" icon={<FileOutlined />} />
       </Menu>
     </Sider>
   );
