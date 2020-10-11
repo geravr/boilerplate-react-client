0d7b5ebb58d758a1a531eeee2d2cacb781474d8d
Gerardo Villa Ramírez <gerardovr@MacBook-Pro.local>
2020-10-11 01:35:28 -0500

    add dev environment mode without strictmode

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import 'antd/dist/antd.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import "antd/dist/antd.css";
+
+const isDev = process.env.REACT_APP_ENVIRONMENT_MODE === "development";
 
 ReactDOM.render(
-  <React.StrictMode>
+  isDev ? (
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  ) : (
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  ),
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
