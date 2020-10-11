71bc09348a5016b525285d22a7fe171c1c5f7997
Gerardo Villa Ramírez <gerardovr@MacBook-Pro.local>
2020-10-10 22:26:27 -0500

    add groups route

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -5,13 +5,15 @@ import { Route, Switch } from "react-router-dom";
 
 // Components
 import PrivateRoute from "./PrivateRoute";
-import UsersContainer from "../pages/Admin/Users/containers/UsersContainer";
 import NotFoundPage from "../pages/NotFoundPage";
+import UsersContainer from "../pages/Admin/Users/containers/UsersContainer";
+import GroupsContainer from "../pages/Admin/Groups/containers/GroupsContainer";
 
 const Routes = () => {
   return (
     <Switch>
       <PrivateRoute path="/admin/users" component={UsersContainer} />
+      <PrivateRoute path="/admin/groups" component={GroupsContainer} />
       <Route component={NotFoundPage} />
     </Switch>
   );
