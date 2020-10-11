9eeb89329a1d999218109060c288a9392af5934d
Gerardo Villa Ramírez <gerardovr@MacBook-Pro.local>
2020-10-10 23:09:05 -0500

    update path components

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -6,8 +6,8 @@ import { Route, Switch } from "react-router-dom";
 // Components
 import PrivateRoute from "./PrivateRoute";
 import NotFoundPage from "../pages/NotFoundPage";
-import UsersContainer from "../pages/Admin/Users/containers/UsersContainer";
-import GroupsContainer from "../pages/Admin/Groups/containers/GroupsContainer";
+import UsersContainer from "../pages/admin/users/containers/UsersContainer";
+import GroupsContainer from "../pages/admin/groups/containers/GroupsContainer";
 
 const Routes = () => {
   return (
