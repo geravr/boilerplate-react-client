4076332dd3240c1d9fbd059bae6747279deaa699
Gerardo Villa Ramírez <gerardovr@MacBook-Pro.local>
2020-10-11 03:14:14 -0500

    add Home route

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -8,10 +8,12 @@ import PrivateRoute from "./PrivateRoute";
 import NotFoundPage from "../pages/NotFoundPage";
 import UsersContainer from "../pages/admin/users/containers/UsersContainer";
 import GroupsContainer from "../pages/admin/groups/containers/GroupsContainer";
+import HomeContainer from "../pages/home/HomeContainer";
 
 const Routes = () => {
   return (
     <Switch>
+      <PrivateRoute exact path="/" component={HomeContainer} />
       <PrivateRoute path="/admin/users" component={UsersContainer} />
       <PrivateRoute path="/admin/groups" component={GroupsContainer} />
       <Route component={NotFoundPage} />
