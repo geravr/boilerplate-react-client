f2f0363ace25777ad5f598e601e744046c23bd33
Gerardo Villa Ramírez <gerardovr@MacBook-Pro.local>
2020-10-16 22:00:26 -0500

    add component for user profile

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Logo from "./Logo";
 
-import { Layout as MainLayout } from "antd";
+// Ant Design
+import { Layout as MainLayout, Row, Col } from "antd";
 
 // Components
 import SideNav from "./SideNav";
 import MainContent from "./MainContent";
+import UserProfileDropDown from "./UserProfileDropDown";
 
 const { Header, Footer } = MainLayout;
 
@@ -13,12 +15,19 @@ const Layout = ({ children, breadcrumb }) => {
   return (
     <MainLayout style={{ minHeight: "100vh" }}>
       <Header className="site-layout-background" style={{ padding: 0 }}>
-        <Logo />
+        <Row justify="space-between">
+          <Col>
+            <Logo />
+          </Col>
+          <Col className="user-avatar">
+            <UserProfileDropDown />
+          </Col>
+        </Row>
       </Header>
       <MainLayout>
         <SideNav />
         <MainLayout className="site-layout">
-          <MainContent breadcrumb={breadcrumb} >{children}</MainContent>
+          <MainContent breadcrumb={breadcrumb}>{children}</MainContent>
           <Footer style={{ textAlign: "center" }}>
             <h4>Boilerplate</h4>
             React - Ant Design - Axios - JWT
