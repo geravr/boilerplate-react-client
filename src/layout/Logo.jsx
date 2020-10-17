2ff7fc0b212b7dcbd11e77ca3cfd90cc4eac30c6
Gerardo Villa Ramírez <gerardovr@MacBook-Pro.local>
2020-10-16 21:10:06 -0500

    add default logo component

--- src/layout/Logo.jsx
+++ src/layout/Logo.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../logo.svg";
 
-const Logo = () => {
+const Logo = ({ height = "20", fontSize = "20px", fontColor = "#f1f1f1"}) => {
   return (
     <div className="logo">
       <Link to="/">
-        <img src={logo} alt="logo" height="20" />
-        <span>React</span>
+        <img src={logo} alt="logo" height={height} />
+        <span style={{fontSize: fontSize, color: fontColor}}>React</span>
       </Link>
     </div>
   );
