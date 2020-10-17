3a3c5d8753cb9928f7f87ae5d4beeb8c2ea664d0
Gerardo Villa Ramírez <gerardovr@MacBook-Pro.local>
2020-10-16 23:04:21 -0500

    import util logout for use in UserProfile comnponent

---
+++ src/layout/UserProfileDropDown.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+// Utils
+import logout from "../utils/logout";
+
+// Ant Design
+import { Menu, Dropdown, Avatar } from "antd";
+import { UserOutlined } from "@ant-design/icons";
+
+const UserProfileDropDown = () => {
+
+  const menu = (
+    <Menu>
+      <Menu.Item disabled>
+        <a>
+          username
+        </a>
+      </Menu.Item>
+      <Menu.Item onClick={logout}>
+        <a>
+          Cerrar sesión
+        </a>
+      </Menu.Item>
+    </Menu>
+  );
+
+  return (
+    <Dropdown overlay={menu} placement="bottomRight">
+      <Avatar size="40" icon={<UserOutlined />} />
+    </Dropdown>
+  );
+};
+
+export default UserProfileDropDown;
