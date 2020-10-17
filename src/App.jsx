cb70a12f1e31d1cd8e97e7b87f7f991faac284c1
Gerardo Villa Ramírez <gerardovr@MacBook-Pro.local>
2020-10-16 21:07:00 -0500

    import and add Routes component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 
+import { initAxiosInterceptors } from "./config/axios";
+
 import Routes from "./routes/Routes";
 
+initAxiosInterceptors();
+
 function App() {
+
   return (
     <Router>
       <Routes />
