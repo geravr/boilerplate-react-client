d31ccac8a363b2c12086a38f0e46d77eda3b68f0
Gerardo Villa <gera.villa21@gmail.com>
2020-10-22 21:54:43 -0500

    add comments

--- src/components/admin/groups/DeleteGroup.jsx
+++ src/components/admin/groups/DeleteGroup.jsx
@@ -8,23 +8,26 @@ import { Modal } from "antd";
 import { ExclamationCircleOutlined } from "@ant-design/icons";
 
 const DeleteGroup = (props) => {
+  /*************** Destructuring ***************/
   const { children, color = "#000", name, id, fetchGroups } = props;
-
   const { confirm } = Modal;
+
+  /*************** Functions ***************/
   function showConfirm() {
     confirm({
       title: `¿Estás seguro que deseas eliminar el grupo ${name}?`,
       icon: <ExclamationCircleOutlined />,
-      content: "Al eliminar un grupo, este se removerá de los usuarios que lo estén usando y se perderán los permisos heredados!",
+      content:
+        "Al eliminar un grupo, este se removerá de los usuarios que lo estén usando y se perderán los permisos heredados!",
       onOk() {
         const deleteUser = async () => {
           try {
-            await axiosClient.delete(`auth/groups/${id}/`)
+            await axiosClient.delete(`auth/groups/${id}/`);
             fetchGroups();
           } catch (error) {
             console.error(error);
           }
-        }
+        };
         deleteUser();
       },
       onCancel() {
